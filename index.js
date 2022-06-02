b81cec8ecd10667ab0ee3114755498b8e95b0f0e
YaraWS <39776511+YaraWS@users.noreply.github.com>
2022-06-02 22:43:54 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let belly = 0
+let result = document.getElementById("result")
+let water = 200
+
+function drinkWater() {
+  belly += 200
+  result.textContent = belly + " ml"
+}
+
+function drinkTea() {
+    belly += 200
+    result.textContent = belly + " ml"
+}
+
+function drinkCoffee() {
+    belly += 200
+    result.textContent = belly + " ml"
+}
+
+function drinkWine() {
+    belly += 200
+    result.textContent = belly + " ml"
+}
